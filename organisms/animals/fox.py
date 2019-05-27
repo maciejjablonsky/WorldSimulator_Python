26562ca86bf6e2f5bf7ef058e6eeba7e960e8c5a
maciek <maciekjablonsky@gmail.com>
2019-05-27 13:16:33 +0200

    Added Fox with own action

---
+++ organisms/animals/fox.py
@@ -0,0 +1,30 @@
+from organisms.animals.animal import Animal
+from random import randrange
+
+
+class Fox(Animal):
+    def __init__(self, world, strength=3, initiative=7, species="Fox"):
+        super(Animal, self).__init__(world, strength, initiative, species)
+
+    def action(self):
+        attacker = self
+        places_to_move = attacker.where_to_move
+        place_to_move = places_to_move[randrange(0, places_to_move.count())].copy()
+        attacker.move_to_destination(place_to_move)
+        while attacker.attack_in_process:
+            if self.world.is_there_anybody(place_to_move):
+                defender = attacker.who_is_defending(place_to_move)
+                if attacker.strength > defender.strength:
+                    defender.collision(attacker)
+                    if attacker.alive and attacker.attack_in_process:
+                        attacker.move_to_destination()
+                else:
+                    self.world.commentator.say(attacker + " dodged fight with " + defender)
+                    places_to_move.remove(place_to_move)
+                    if places_to_move.count() > 0:
+                        place_to_move = places_to_move[randrange(0, places_to_move.count())].copy()
+                        attacker.destination = place_to_move
+                    else:
+                        attacker.destination = attacker.position
+            else:
+                attacker.move_to_destination()
