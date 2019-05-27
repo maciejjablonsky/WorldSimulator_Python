c4efa90a39b86fed0ab2d5dc79acc94943fc9727
maciek <maciekjablonsky@gmail.com>
2019-05-27 13:15:32 +0200

    Changed tab to 4 spaces

--- set_get_appender.py
+++ set_get_appender.py
@@ -3,10 +3,10 @@ import sys
 file = open(sys.argv[1], "a")
 var_name = sys.argv[2]
 if file.writable():
-    file.write("\n\t@property")
-    file.write("\n\tdef {0:s}(self):".format(var_name))
-    file.write("\n\t\treturn self._{0:s}\n".format(var_name))
-    file.write("\n\t@{0:s}.setter".format(var_name))
-    file.write("\n\tdef {0:s}(self, {0:s}):".format(var_name))
-    file.write("\n\t\tself._{0:s} = {0:s}\n".format(var_name))
+    file.write("\n    @property")
+    file.write("\n    def {0:s}(self):".format(var_name))
+    file.write("\n        return self._{0:s}\n".format(var_name))
+    file.write("\n    @{0:s}.setter".format(var_name))
+    file.write("\n    def {0:s}(self, {0:s}):".format(var_name))
+    file.write("\n        self._{0:s} = {0:s}\n".format(var_name))
     file.close()
