c6082999bfa19e93e75de093c97b4a9460d20b0d
maciek <maciekjablonsky@gmail.com>
2019-05-27 21:06:31 +0200

    Fixed antelope species assignment

--- species.py
+++ species.py
@@ -1,11 +1,31 @@
-species = {
-    "Wolf": {
+species_dictionary = {
+    "antelope": {
+        "class": "Antelope",
+        "prompt": "hoop"
+    },
+    "cyber sheep": {
+        "class": "CyberSheep",
+        "prompt": "MEEE"
+    },
+    "fox": {
+        "class": "Fox",
+        "prompt": "bork"
+    },
+    "human": {
+        "class": "Human",
+        "prompt": "auuu"
+    },
+    "sheep": {
+        "class": "Sheep",
+        "prompt": "meee"
+    },
+    "turtle": {
+        "class": "Turtle",
+        "prompt": "..."
+    },
+    "wolf": {
         "class": "Wolf",
         "prompt": "woof"
     },
-    "Sheep": {
-        "class": "Sheep",
-        "prompt": "meee"
-    }
-    ""
+
 }
